0db5999d7e9c18359d2b3e327ea77bb235fffbcb
lawrencemboya <lawrencemboya@gmail.com>
2019-07-17 23:00:47 -0400

    getting started

---
+++ src/main/java/wcci/moviemasteryproject/MovieMasteryProjectApplication.java
@@ -0,0 +1,13 @@
+package wcci.moviemasteryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMasteryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMasteryProjectApplication.class, args);
+	}
+
+}
